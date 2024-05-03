882bb45a03b54babe359d86dfb0bf69e8bb4165b
Yogesh0209 <ybhadauriya40@gmail.com>
2024-05-03 22:18:44 +0530

    Cpp project

---
+++ new.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int a,b;
+    cout<<"Give two numbers for addition :";
+    cin>>a>>b;
+    cout<<"Your answer is : "<<a+b;
+    return 0;
+}
